8a08d25c572fc020d4a60bc9a2ddbdcf91474566
hemant <hemantarya7860@gmail.com>
2024-10-15 17:28:09 +0530

    bug : system theme color bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,10 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="flex  mt-2  py-2 px-2  justify-between  sticky top-0 ">
+    <nav className="flex    py-2 px-2  justify-between  sticky top-0 z-50 bg-gray-400">
       <section className="	 ">
         <Link href="/">
+          {" "}
           <img
             className="h-16  w-full "
             src="https://thumbs.dreamstime.com/b/ready-summer-vacation-travel-background-d-rendering-114574299.jpg"
@@ -17,7 +18,7 @@ function Navbar() {
         <div>
           <input
             placeholder="Search"
-            className="py-2 px-2 text-black border w-96 rounded-md	"
+            className="py-2 px-2 text-black w-96 rounded-md border bg	"
           />
         </div>
         <div>
